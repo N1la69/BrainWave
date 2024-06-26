a9b1955591e46dc43ea89dd99cc0c14898e67ade
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2024-06-26 11:39:29 +0530

    final sections added

--- brainwave/src/App.jsx
+++ brainwave/src/App.jsx
@@ -1,9 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benifits from "./components/Benifits";
 import Collab from "./components/Collab";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import RoadMap from "./components/RoadMap";
 import Services from "./components/Services";
 
 const App = () => {
@@ -16,6 +18,8 @@ const App = () => {
         <Collab />
         <Services />
         <Pricing />
+        <RoadMap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
